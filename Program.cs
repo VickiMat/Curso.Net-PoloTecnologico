baf898133caba1f3e10d4a36480005b1e11a9a42
vicki <vickimathurin94@gmail.com>
2022-08-31 15:56:30 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string userAnswer = "Y";
+
+        while (userAnswer == "Y")
+        {
+            Console.Clear();
+
+            Console.WriteLine("Please, insert your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}!");
+
+            Console.WriteLine("Do you want to continue? Y/N");
+            userAnswer = Console.ReadLine().ToUpper();
+        }
+        if (userAnswer == "N") Console.WriteLine("Program completed successfully");
+        else Console.WriteLine("Option invalid");
+    }
+}
