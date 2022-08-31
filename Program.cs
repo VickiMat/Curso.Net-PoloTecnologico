de911e53d2729fbff5138168de827da315e6beba
vicki <vickimathurin94@gmail.com>
2022-08-31 20:15:18 -0300

    Program guess the number ready

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to GUESS A NUMBER! You have 10 attemps to acert the correct number");
+            int secretNumber = new Random(DateTime.Now.Millisecond).Next(1, 21);
+            int attempts = 10;
+
+            for (int i = 0; i < 10; i++)
+			{
+                Console.WriteLine("Please, enter one number between 1-20 to see if you guess the secret number ;)");
+                int guessNumber = Int32.Parse(Console.ReadLine());
+                    if (guessNumber > secretNumber)
+                    {
+                    Console.WriteLine("The number is too big!");
+                    attempts--;
+                    }
+                    else if (guessNumber < secretNumber && attempts>0)
+                    {
+                    Console.WriteLine("The number is too low!");
+                    attempts--;
+                    }
+                    if (guessNumber == secretNumber)
+                    {
+                        Console.WriteLine($"Congratulations! You have guessed the secret number! It was {secretNumber}");
+                        Console.WriteLine($"You did it in {11-attempts} tries!!!");
+                    break;
+                    }
+			}
+            if(attempts == 0)
+            {
+                Console.WriteLine("You don´t have more attemps available");
+                Console.WriteLine("Press a key to close the program");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Press a key to close the program");
+                Console.ReadKey();
+            }  
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Clase06
+
+Consigna
+
+1) Generar un número secreto
+aleatorio con la siguiente instruccion:
+
+// Genera un numero entero aleatorio de 1 a 20
+int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+
+2) Pedir al usuario que ingrese un número y que
+intente adivinar el número que eligió la computadora.
+
+3) Si el numero ingresado por el usuario es mayor
+al número secreto, avisarle que es muy grande y que intente de nuevo y que
+vuelva al paso 2.
+
+4) Si el numero ingresado es menor al número
+secreto, avisarle que es muy chico, y que intente de nuevo volviendo al paso 2.
+
+5) Si el número ingresado coincide con el número
+secreto, sacar el siguiente mensaje:
+
+"Felicitaciones, has adivinado el número
+secreto que era:  [numeroSecreto]"
+"Lo has logrado en [n] intentos!!"
+
+Y finalizar el programa.
