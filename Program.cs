b55773a267661c141c908b3b35788c72e7803388
vicki <vickimathurin94@gmail.com>
2022-09-01 02:38:19 -0300

    Consignas

--- Program.cs
+++ Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            
+            //Input the numbers
+            Console.WriteLine("Hello, enter 10 numbers and you will get some information about them");
+            int[] numbersEntered = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Input your number {i+1}");
+                numbersEntered[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Show all the numbers
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Your number {i+1} is: {numbersEntered[i]}");
+            }
+
+            //Sum all numbers
+            int sum = 0;
+            for (int i = 0; i < numbersEntered.Length; i++)
+            {
+                sum += numbersEntered[i];
+            }
+            Console.WriteLine($"The sum of all your numbers is {sum}");
+
+            //Max number
+            Console.WriteLine($"The higher number in your list is {numbersEntered.Max()}");
+
+            //Min number
+            Console.WriteLine($"The lower number in your list is {numbersEntered.Min()}");
+
+            //average number
+            int averageNumber = sum / 10;
+            Console.WriteLine($"The average of your numbers is {averageNumber}");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Clase07
+
+# Consigna
+
+Escribe un programa que:
+
+1) Pida 10 números al usuario.
+2) Obtener la suma de todos los números ingresados.
+3) Obtener cual es el mayor de todos los números.
+4) Obtener cual es el menor de todos los números.
+5) Obtener el promedio de todos los números.
+6) Mostrar todos los números ingresados por pantalla.
+7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.
